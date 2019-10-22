9b55b96f0e0c3dd0713d3712c97e27cdbfd32e9d
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-10-22 10:45:40 -0400

    banner for promise

--- penguins.js
+++ penguins.js
@@ -1,1 +1,23 @@
+var setBanner = function(message)
+{
+    d3.select("#banner").text(message);
+}
 
+
+
+
+
+
+
+
+var penguinPromise = d3.json("classData.json")
+
+penguinPromise.then(function(penguin)
+{
+ setBanner("Pretty Pompous Penguins");
+ drawImages(penguin);
+},
+function(err)
+{
+ setBanner("Penguins Have Left the Building");
+});
