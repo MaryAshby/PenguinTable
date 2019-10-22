5ce748a6ca63c36d81e559976b1266a0af225503
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-10-22 11:00:35 -0400

    variables

--- penguins.js
+++ penguins.js
@@ -1,3 +1,41 @@
+//creation of requested data//
+var picture=function(penguin)
+{var obj=
+    {picture: penguin.picture};
+     return obj;}
+
+var getGrade=function(quiz) 
+{return quiz.grade;}
+
+var meanQuiz = function(penguin)
+{var pq=penguin.quizes.map(getGrade);
+ var answer=d3.mean(pq);
+ return answer;}
+
+var hwGrade=function(homework)
+{return answer;}
+
+var meanHW=function(penguin)
+{var pq=penguin.homework.map(hwGrade);
+ var answer=d3.mean(pq);
+ return answer;}
+
+var testGrade=funtion(test)
+{return test.grade;}
+
+var meanTests=function(penguin)
+{var pq=penguin.test.map(testGrade);
+ var answer=d3.mean(pq);
+ return answer;}
+
+var finalGrade=function(oebgyub)
+{var obj=(meanFinal*0.35)+(
+//push of new info to penguins//
+
+
+
+
+//set banner for promise//
 var setBanner = function(message)
 {
     d3.select("#banner").text(message);
@@ -9,7 +47,7 @@ var setBanner = function(message)
 
 
 
-
+//promise//
 var penguinPromise = d3.json("classData.json")
 
 penguinPromise.then(function(penguin)
